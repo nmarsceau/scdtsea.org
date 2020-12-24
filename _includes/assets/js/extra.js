ca401a6f27cf0401fd598715abfd985dcc1a0a3a
nmarsceau <nickmarsceau@gmail.com>
2020-12-23 22:32:38 -0500

    Update back button behavior when no history is available

--- _includes/assets/js/extra.js
+++ _includes/assets/js/extra.js
@@ -19,3 +19,13 @@
     copyright_current_year.innerText = (new Date()).getFullYear();
   }
 })();
+
+(function() {
+  const news_back_buttons = document.querySelectorAll('.news-back-button');
+  news_back_buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      if (window.history.length === 1) {window.location.href = '/news';}
+      else {window.history.back();}
+    });
+  });
+})();
